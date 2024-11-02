40b3c14f30647a49dff2cac1aa00924cb78d0855
Jocelyn Miranda <liareal@gmail.com>
2024-11-01 19:07:01 -0500

    feat: adds header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,50 @@
-import { h } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style.css';
+import { h } from "preact";
+import { Link } from "preact-router/match";
+import { AppBar, Badge, IconButton, Toolbar, Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ShoppingCart from "@mui/icons-material/ShoppingCart";
+import style from "./style.css";
 
 const Header = () => (
-	<header class={style.header}>
-		<a href="/" class={style.logo}>
-			<img src="../../assets/logo-inverse.svg" alt="Preact Logo" height="32" width="32" />
-			<h1>Preact CLI</h1>
-		</a>
-		<nav>
-			<Link activeClassName={style.active} href="/">
-				Home
-			</Link>
-			<Link activeClassName={style.active} href="/profile">
-				Me
-			</Link>
-			<Link activeClassName={style.active} href="/profile/john">
-				John
-			</Link>
-		</nav>
-	</header>
+  <AppBar position="fixed">
+    <Toolbar>
+      <Link href="/">
+        <img
+          src="../../assets/logo-inverse.svg"
+          alt="Store Logo"
+          className={style.img}
+          height="32"
+          width="32"
+          loading="lazy"
+        />
+      </Link>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Mobi Store
+      </Typography>
+      <IconButton
+        aria-label="cart"
+        size="large"
+        edge="start"
+        color="inherit"
+        title="Cart"
+      >
+        <Badge badgeContent={1} color="secondary">
+          <ShoppingCart />
+        </Badge>
+      </IconButton>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="Github project"
+        title="Github project"
+        sx={{ ml: 2 }}
+        href="https://github.com/jocammir/frontend_test.git"
+      >
+        <GitHubIcon />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Header;
